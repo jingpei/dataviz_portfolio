f28858f850d2ac37b997d2515a5c8ad920bd306c
jingpei <jingjpei@gmail.com>
2015-04-23 21:28:50 -0400

    working single page with routeParams

--- 2-portfolio/app/app.js
+++ 2-portfolio/app/app.js
@@ -5,5 +5,6 @@ var PortfolioListController = angular.module('PortfolioListController', []);
 portfolioApp.config(['$routeProvider', function( $routeProvider ){
 	$routeProvider
 		.when('/', { templateUrl: "partials/portfolios.html", controller: "PortfolioListController" })
+		.when('/portfolio/:portfolio_name', { templateUrl: "partials/portfolio_viewer.html", controller: "PortfolioViewerController" })
 		.otherwise({ redirectTo: "/404" })
 }])
